ea80bf2dfba833fd03b30f1d09203e7cd19b8e59
Dmitriy Don <dmitriysdon@gmail.com>
2020-05-22 15:48:47 +0300

    mixins: raise ServerError with kwargs only

--- src/httpapiclient/mixins.py
+++ src/httpapiclient/mixins.py
@@ -41,7 +41,7 @@ class JsonSchemaResponseMixin(JsonResponseMixin):
         try:
             Draft4Validator(schema).validate(result)
         except ValidationError as e:
-            raise self.ServerError(e, schema=schema, level='json')
+            raise self.ServerError(description=str(e), schema=schema, level='json')
 
         return result
 
